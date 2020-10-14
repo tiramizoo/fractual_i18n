fd0767a272f0fa6a54a45ba8a64561c9d74747e0
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2020-10-14 15:26:43 +0200

    Add rake tasks for joining and splitting translation files

---
+++ lib/fractual_i18n/phrases.rb
@@ -0,0 +1,34 @@
+class FractualI18n::Phrases
+  def joined(locale:)
+    original_load_path = I18n.load_path
+
+    I18n.load_path = fractual_files
+    I18n.with_locale(locale) { I18n.t(".") }
+  ensure
+    I18n.load_path = original_load_path
+  end
+
+  def store(translations, locale:)
+    fractual_files.each do |filename|
+      content = YAML.load_file(filename)
+
+      fractual_path = FractualI18n.configuration.fractual_paths.find { |path| filename.starts_with?(path) }
+      keys = filename.delete_prefix(fractual_path).delete_prefix("/").split("/")
+      last_key = keys.pop.delete_suffix(".yml")
+      keys << last_key
+      keys.map! { |key| key.delete_prefix("_") } # remove underscore from partial name
+
+      content[locale.to_s] = content.fetch(locale.to_s, {}).merge(translations.dig(locale.to_s, *keys))
+
+      File.write(filename, content.to_yaml(line_width: 200))
+    end
+  end
+
+  private
+
+  def fractual_files
+    I18n.load_path.select do |path|
+      FractualI18n.configuration.fractual_paths.any? { |fp| path.starts_with?(fp) }
+    end
+  end
+end
--- lib/fractual_i18n/railtie.rb
+++ lib/fractual_i18n/railtie.rb
@@ -4,4 +4,8 @@ class FractualI18n::Railtie < Rails::Railtie
   initializer "fractual_i18n.set_fractual_paths" do |app|
     FractualI18n.configuration.fractual_paths = app.config.paths["app/views"].existent
   end
+
+  rake_tasks do
+    load "tasks/fractual_i18n.rake"
+  end
 end
